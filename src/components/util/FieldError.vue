c166b10a47f6f25752e6bcc5c3efbf7ec532ae0e
efrain105 <efrainclochard@hotmail.com>
2026-04-04 14:29:37 -0600

    fix(forms): remove redundant "Error:" label in `FieldError` component
    Cleans up the `FieldError` component by removing the hardcoded "Error:" label, leaving only the dynamic error message for improved clarity and flexibility in error display.

--- src/components/util/FieldError.vue
+++ src/components/util/FieldError.vue
@@ -4,7 +4,7 @@
       <svg class="w-3 h-3 mr-1 flex-shrink-0" fill="currentColor" viewBox="0 0 20 20">
         <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z" clip-rule="evenodd"></path>
       </svg>
-      <span class="font-medium">Error:</span>
+      <span class="font-medium"></span>
       <span class="ml-1">{{ error }}</span>
     </p>
   </div>
